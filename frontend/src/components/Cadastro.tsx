0dd25aca565a469d96c1a3264e99d452fd562cea
samcamargoo <sam6camargo@gmail.com>
2022-09-19 15:06:32 -0300

    tela de cadastro aprimorada

--- frontend/src/components/Cadastro.tsx
+++ frontend/src/components/Cadastro.tsx
@@ -1,9 +1,13 @@
 import {
   Button,
+  Center,
+  Divider,
   Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
+  Heading,
+  Hide,
   HStack,
   Input,
   Text,
@@ -45,175 +49,182 @@ export function Cadastro() {
   return (
     <>
       <Navbar />
-      <Flex justifyContent="center" alignItems="center" mb={2}>
-        
-          <form onSubmit={handleSubmit(cadastrar)}>
-            <FormControl>
-              <FormLabel>Nome*</FormLabel>
-              <Input
-                type="text"
-                placeholder="Nome"
-                {...register("nome", {
-                  required: 'O campo "Nome" é obrigatório',
-                  minLength: {
-                    value: 2,
-                    message:
-                      'O campo "Nome" precisa ter pelo menos 2 caracteres',
-                  },
-                })}
-              />
-            </FormControl>
-            <ErrorMessage
-              errors={errors}
-              name="nome"
-              render={({ messages }) =>
-                messages &&
-                Object.entries(messages).map(([type, message]) => (
-                  <Text fontSize="10px" color="red" key={type}>
-                    {message}
-                  </Text>
-                ))
-              }
-            />
-
-            <FormControl>
-              <FormLabel>Email*</FormLabel>
-              <Input
-                type="text"
-                placeholder="Email"
-                {...register("email", {
-                  required: 'O campo "Email" é obrigatório',
-                  pattern: {
-                    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                    message: "Email inválido",
-                  },
-                })}
-              />
-            </FormControl>
-            <ErrorMessage
-              errors={errors}
-              name="email"
-              render={({ messages }) =>
-                messages &&
-                Object.entries(messages).map(([type, message]) => (
-                  <Text fontSize="10px" color="red" key={type}>
-                    {message}
-                  </Text>
-                ))
-              }
-            />
-
-            <FormControl>
-              <FormLabel>CPF*</FormLabel>
-              <Input
-                as={InputMask}
-                mask="999.999.999-99"
-                type="text"
-                placeholder="CPF"
-                {...register("cpf", {
-                  required: 'O campo "CPF" é obrigatório',
-                })}
-              />
-            </FormControl>
-            <ErrorMessage
-              errors={errors}
-              name="cpf"
-              render={({ messages }) =>
-                messages &&
-                Object.entries(messages).map(([type, message]) => (
-                  <Text fontSize="10px" color="red" key={type}>
-                    {message}
-                  </Text>
-                ))
-              }
-            />
-            <FormControl>
-              <FormLabel>Data de Nascimento*</FormLabel>
-              <Input
-                type="date"
-                placeholder="Data de Nascimento"
-                {...register("dataNascimento", {
-                  required: 'O campo "Data de Nascimento" é obrigatório',
-                })}
-              />
-            </FormControl>
-            <ErrorMessage
-              errors={errors}
-              name="dataNascimento"
-              render={({ messages }) =>
-                messages &&
-                Object.entries(messages).map(([type, message]) => (
-                  <Text fontSize="10px" color="red" key={type}>
-                    {message}
-                  </Text>
-                ))
-              }
-            />
-            <FormControl>
-              <FormLabel>Senha*</FormLabel>
-              <Input
-                type="password"
-                placeholder="Senha"
-                {...register("password", {
-                  required: 'O campo "Senha" é obrigatório',
-                  minLength: {
-                    value: 3,
-                    message: "A senha deve ter no mínimo 3 caracteres",
-                  },
-                })}
-              />
-            </FormControl>
-            <ErrorMessage
-              errors={errors}
-              name="password"
-              render={({ messages }) =>
-                messages &&
-                Object.entries(messages).map(([type, message]) => (
-                  <Text fontSize="10px" color="red" key={type}>
-                    {message}
-                  </Text>
-                ))
-              }
-            />
-            <FormControl>
-              <FormLabel>Confirmar senha*</FormLabel>
-              <Input
-                type="password"
-                placeholder="Confirmar senha"
-                {...register("confirma_password", {
-                  validate: (value) =>
-                    password.current === value || "Senhas não coincidem",
-                })}
-              />
-            </FormControl>
-            <ErrorMessage
-              errors={errors}
-              name="confirma_password"
-              render={({ messages }) =>
-                messages &&
-                Object.entries(messages).map(([type, message]) => (
-                  <Text fontSize="10px" color="red" key={type}>
-                    {message}
-                  </Text>
-                ))
-              }
-            />
-            <Flex mt={2}>
-              <HStack>
-                <Button colorScheme="red">Cancelar</Button>
-
-                <Button
-                  colorScheme="teal"
-                  type="submit"
-                  isLoading={buttonDisable}
-                >
-                  Cadastrar
-                </Button>
-              </HStack>
-            </Flex>
-          </form>
+      <Flex justifyContent={["center", "center", "center", "space-around"]} alignItems="center" height="100%" p={4}>
+        <Hide below="lg">
+        <Flex maxWidth="400px" flexDir="column">
+          <Heading fontSize="20px">Vantagens de ter uma conta no Mock Banco Digital</Heading>
+          <Text mt={2}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper posuere tempor. Morbi sodales eros nunc, vel convallis mi scelerisque at. Integer blandit tortor quis cursus consectetur. Ut ac lacus eget sapien faucibus gravida. Sed mattis lectus augue, non hendrerit dui imperdiet eu. Integer ac nibh odio. Mauris rhoncus accumsan sagittis.</Text>
         </Flex>
-      
+        <Flex height="300px">
+          <Divider orientation="vertical"/>
+        </Flex>
+        </Hide>
+        <form onSubmit={handleSubmit(cadastrar)}>
+          <FormControl>
+            <FormLabel>Nome*</FormLabel>
+            <Input
+            width={["max-content", "200px", "200px", "400px"]}
+              type="text"
+              placeholder="Nome"
+              {...register("nome", {
+                required: 'O campo "Nome" é obrigatório',
+                minLength: {
+                  value: 2,
+                  message: 'O campo "Nome" precisa ter pelo menos 2 caracteres',
+                },
+              })}
+            />
+          </FormControl>
+          <ErrorMessage
+            errors={errors}
+            name="nome"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <Text fontSize="10px" color="red" key={type}>
+                  {message}
+                </Text>
+              ))
+            }
+          />
+
+          <FormControl>
+            <FormLabel>Email*</FormLabel>
+            <Input
+              type="text"
+              placeholder="Email"
+              {...register("email", {
+                required: 'O campo "Email" é obrigatório',
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                  message: "Email inválido",
+                },
+              })}
+            />
+          </FormControl>
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <Text fontSize="10px" color="red" key={type}>
+                  {message}
+                </Text>
+              ))
+            }
+          />
+
+          <FormControl>
+            <FormLabel>CPF*</FormLabel>
+            <Input
+              as={InputMask}
+              mask="999.999.999-99"
+              type="text"
+              placeholder="CPF"
+              {...register("cpf", {
+                required: 'O campo "CPF" é obrigatório',
+              })}
+            />
+          </FormControl>
+          <ErrorMessage
+            errors={errors}
+            name="cpf"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <Text fontSize="10px" color="red" key={type}>
+                  {message}
+                </Text>
+              ))
+            }
+          />
+          <FormControl>
+            <FormLabel>Data de Nascimento*</FormLabel>
+            <Input
+              type="date"
+              placeholder="Data de Nascimento"
+              {...register("dataNascimento", {
+                required: 'O campo "Data de Nascimento" é obrigatório',
+              })}
+            />
+          </FormControl>
+          <ErrorMessage
+            errors={errors}
+            name="dataNascimento"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <Text fontSize="10px" color="red" key={type}>
+                  {message}
+                </Text>
+              ))
+            }
+          />
+          <FormControl>
+            <FormLabel>Senha*</FormLabel>
+            <Input
+              type="password"
+              placeholder="Senha"
+              {...register("password", {
+                required: 'O campo "Senha" é obrigatório',
+                minLength: {
+                  value: 3,
+                  message: "A senha deve ter no mínimo 3 caracteres",
+                },
+              })}
+            />
+          </FormControl>
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <Text fontSize="10px" color="red" key={type}>
+                  {message}
+                </Text>
+              ))
+            }
+          />
+          <FormControl>
+            <FormLabel>Confirmar senha*</FormLabel>
+            <Input
+              type="password"
+              placeholder="Confirmar senha"
+              {...register("confirma_password", {
+                validate: (value) =>
+                  password.current === value || "Senhas não coincidem",
+              })}
+            />
+          </FormControl>
+          <ErrorMessage
+            errors={errors}
+            name="confirma_password"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <Text fontSize="10px" color="red" key={type}>
+                  {message}
+                </Text>
+              ))
+            }
+          />
+          <Flex mt={2}>
+            <HStack>
+              <Button colorScheme="red">Cancelar</Button>
+
+              <Button
+                colorScheme="teal"
+                type="submit"
+                isLoading={buttonDisable}
+              >
+                Cadastrar
+              </Button>
+            </HStack>
+          </Flex>
+        </form>
+      </Flex>
     </>
   );
 }
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -20,25 +20,25 @@ import "../assets/styles.css";
 export function Footer() {
   return (
     <>
-      <Flex justifyContent="space-around" alignItems="center" backgroundColor="teal" flexDir={["column", "column", "column", "row"]}>
+      <Flex justifyContent="space-around" alignItems="center" backgroundColor="teal" flexDir={["column", "column", "column", "row"]} color="white" p={10}>
         <VStack>
           <Heading fontSize="sm">produtos</Heading>
-          <Text>testando</Text>
-          <Text>testando</Text>
-          <Text>testando</Text>
+          <Text>conta</Text>
+          <Text>cartão de crédito</Text>
+          <Text>empréstimos</Text>
         </VStack>
         <VStack>
-          <Heading fontSize="sm">produtos</Heading>
-          <Text>testando</Text>
-          <Text>testando</Text>
+          <Heading fontSize="sm">Mock Banco Digital</Heading>
+          <Text>sobre o projeto</Text>
+          <Text>sobre o desenvolvedor</Text>
           <Text>testando</Text>
         </VStack>
 
         <VStack>
           <Heading fontSize="sm">produtos</Heading>
-          <Text>testando</Text>
-          <Text>testando</Text>
-          <Text>testando</Text>
+          <Link href="https://api.whatsapp.com/send?phone=5534991155634" isExternal>envie uma mensagem</Link>
+          <Link href="https://linkedin.com/in/samuel-camargo" isExternal>linkedin</Link>
+          <Link href="https://github.com/samcamargoo" isExternal>github</Link>
         </VStack>
       </Flex>
     </>
